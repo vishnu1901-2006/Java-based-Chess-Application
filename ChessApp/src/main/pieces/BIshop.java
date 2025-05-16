dc23dadcff3fe50e961751a987798fef5788d7b3
Vishnukant Bajpai <bajpaivishnukant237@gmail.com>
2025-05-16 18:40:48 +0530

    Update BIshop.java

--- ChessApp/src/main/pieces/BIshop.java
+++ ChessApp/src/main/pieces/BIshop.java
@@ -2,52 +2,56 @@ package main.pieces;
 
 import main.board.Board;
 import main.board.Square;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class BIshop extends Piece {
-    public BIshop(Color color) {
+public class Bishop extends Piece {
+
+    public Bishop(Color color) {
         super(color);
     }
 
     @Override
-    public List<Square> getLegalMoves(Board board, Square currentSquare) {
+    public List<Square> getLegalMoves(Board board, Square startSquare) {
         List<Square> legalMoves = new ArrayList<>();
-        int row = currentSquare.getRow();
-        int col = currentSquare.getCol();
+        int startRow = startSquare.getRow();
+        int startCol = startSquare.getCol();
 
-        // Directions: up-left, up-right, down-left, down-right
-        int[][] directions = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        // Directions: Up-Right, Up-Left, Down-Right, Down-Left
+        int[][] directions = {{-1, 1}, {-1, -1}, {1, 1}, {1, -1}};
 
         for (int[] dir : directions) {
-            int currentRow = row + dir[0];
-            int currentCol = col + dir[1];
+            int rowDir = dir[0];
+            int colDir = dir[1];
 
-            while (isValidSquare(currentRow, currentCol)) {
-                Square targetSquare = board.getSquare(currentRow, currentCol);
+            for (int i = 1; i < 8; i++) { // Iterate up to 7 squares in each direction
+                int targetRow = startRow + i * rowDir;
+                int targetCol = startCol + i * colDir;
+
+                if (!isWithinBounds(targetRow, targetCol)) {
+                    break; // Out of bounds, stop checking in this direction
+                }
+
+                Square targetSquare = board.getSquare(targetRow, targetCol);
+                 if (targetSquare == null) { // Safeguard
+                    break;
+                }
+
                 if (targetSquare.isEmpty()) {
                     legalMoves.add(targetSquare);
                 } else {
-                    if (targetSquare.getPiece().getColor() != getColor()) {
+                    if (targetSquare.getPiece().getColor() != this.getColor()) {
                         legalMoves.add(targetSquare); // Capture opponent's piece
                     }
-                    break; // Stop in this direction after encountering a piece
+                    break; // Blocked by own piece or captured opponent's piece, stop checking in this direction
                 }
-                currentRow += dir[0];
-                currentCol += dir[1];
             }
         }
-
         return legalMoves;
-    }
-
-    private boolean isValidSquare(int row, int col) {
-        return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
 
     @Override
     public String toString() {
-        return (getColor() == Color.WHITE ? "W" : "B") + "_Bishop";
+        return (color == Color.WHITE) ? "W_Bishop" : "B_Bishop";
     }
 }
